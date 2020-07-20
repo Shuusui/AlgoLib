f9ebe636065356aab952eb73cc61f82819cd2b08
Unknown <K.schaefer92@gmx.de>
2020-07-20 21:06:15 +0200

    [ UPDATE ] sort method, predicates and new formatting

--- AlgoTest/test.cpp
+++ AlgoTest/test.cpp
@@ -2,23 +2,49 @@
 #include "sort.h"
 
 TEST(SelectionSort, CheckResult) {
-	int arr[6] = {3, 2, 7, 5, 6, 1};
-	AlgoLib::SelectionSort<int>(arr, 6);
-	int sortedArr[6] = {1,2,3,5,6,7};
+	int ascArr[6] = {3, 2, 7, 5, 6, 1};
+	AlgoLib::SelectionSort(ascArr, 6, ESortMethod::Ascending);
+	int ascSortedArr[6] = {1,2,3,5,6,7};
 
 	for (int i = 0; i < 6; ++i)
 	{
-		EXPECT_TRUE(arr[i] == sortedArr[i]);
+		EXPECT_TRUE(ascArr[i] == ascSortedArr[i]);
+	}
+
+	int descArr[6] = { 3, 2, 7, 5, 6, 1 };
+	AlgoLib::SelectionSort(descArr, 6, ESortMethod::Descending);
+	int descSortedArr[6] = { 7,6,5,3,2,1 };
+	for (int i = 0; i < 6; ++i)
+	{
+		EXPECT_TRUE(descArr[i] == descSortedArr[i]);
+	}
+
+	int predArr[6] = { 3, 2, 7, 5, 6, 1 };
+	AlgoLib::SelectionSort<int>(predArr, 6, [](int a, int b)->bool
+		{
+			return a > b;
+		});
+	int predSortedArr[6] = { 1,2,3,5,6,7};
+	for (int i = 0; i < 6; ++i)
+	{
+		EXPECT_TRUE(predArr[i] == predSortedArr[i]);
 	}
 }
 
 TEST(BubbleSort, CheckResult) {
-	int arr[6] = { 3, 2, 7, 5, 6, 1 };
-	AlgoLib::BubbleSort<int>(arr, 6);
-	int sortedArr[6] = { 1,2,3,5,6,7 };
-
+	int ascArr[6] = { 3, 2, 7, 5, 6, 1 };
+	AlgoLib::BubbleSort(ascArr, 6, ESortMethod::Ascending);
+	int ascSortedArr[6] = { 1,2,3,5,6,7 };
 	for (int i = 0; i < 6; ++i)
 	{
-		EXPECT_TRUE(arr[i] == sortedArr[i]);
+		EXPECT_TRUE(ascArr[i] == ascSortedArr[i]);
+	}
+
+	int descArr[6] = { 3, 2, 7, 5, 6, 1 };
+	AlgoLib::BubbleSort(descArr, 6, ESortMethod::Descending);
+	int descSortedArr[6] = { 7,6,5,3,2,1 };
+	for (int i = 0; i < 6; ++i)
+	{
+		EXPECT_TRUE(descArr[i] == descSortedArr[i]);
 	}
 }
--- include/sort.h
+++ include/sort.h
@@ -1,43 +1,111 @@
 #pragma once
 
+enum class ESortMethod
+{
+	Ascending, 
+	Descending
+};
+
 namespace AlgoLib
 {
 	template <typename T>
-	static void SelectionSort(T arr[], const size_t& size)
+	static void SelectionSort(T _arr[], const size_t& _size, const ESortMethod& _method)
 	{
-		for (size_t i = 0; i < size; ++i)
+		for (size_t i = 0; i < _size; ++i)
 		{
 			size_t minIndex = i;
-			for (size_t j = i+1; j < size; ++j)
+			for (size_t j = i + 1; j < _size; ++j)
 			{
-				if (arr[minIndex] < arr[j])
+				bool bMatchMethod = _method == ESortMethod::Ascending ? _arr[minIndex] > _arr[j] : _arr[minIndex] < _arr[j];
+				if (!bMatchMethod)
 				{
 					continue;
 				}
 				minIndex = j;
 			}
-			T tempElement = arr[i];
-			arr[i] = arr[minIndex];
-			arr[minIndex] = tempElement;
+			T tempElement = std::move(_arr[i]);
+			_arr[i] = _arr[minIndex];
+			_arr[minIndex] = tempElement;
 		}
 	}
 
 	template <typename T>
-	static void BubbleSort(T arr[], const size_t& size)
+	static void SelectionSort(T _arr[], const size_t& _size, bool (*_predicate)(T a, T b))
+	{
+		for (size_t i = 0; i < _size; ++i)
+		{
+			size_t minIndex = i;
+			for (size_t j = i + 1; j < _size; ++j)
+			{
+				if (!_predicate(_arr[minIndex], _arr[j]))
+				{
+					continue;
+				}
+				minIndex = j;
+			}
+			T tempElement = std::move(_arr[i]);
+			_arr[i] = _arr[minIndex];
+			_arr[minIndex] = tempElement;
+		}
+	}
+
+	template <typename T>
+	static void SelectionSort(T _arr[], const size_t& _size, bool (*_predicate)(const T& a, const T& b))
+	{
+		for (size_t i = 0; i < _size; ++i)
+		{
+			size_t minIndex = i;
+			for (size_t j = i + 1; j < _size; ++j)
+			{
+				if (!_predicate(_arr[minIndex], _arr[j]))
+				{
+					continue;
+				}
+				minIndex = j;
+			}
+			T tempElement = std::move(_arr[i]);
+			_arr[i] = _arr[minIndex];
+			_arr[minIndex] = tempElement;
+		}
+	}
+
+	template <typename T>
+	static void SelectionSort(T _arr[], const size_t& _size, bool (*_predicate)(T& a, T& b))
+	{
+		for (size_t i = 0; i < _size; ++i)
+		{
+			size_t minIndex = i;
+			for (size_t j = i + 1; j < _size; ++j)
+			{
+				if (!_predicate(_arr[minIndex], _arr[j]))
+				{
+					continue;
+				}
+				minIndex = j;
+			}
+			T tempElement = std::move(_arr[i]);
+			_arr[i] = _arr[minIndex];
+			_arr[minIndex] = tempElement;
+		}
+	}
+
+	template <typename T>
+	static void BubbleSort(T _arr[], const size_t& _size, const ESortMethod& _method)
 	{
 		bool bIsSorted = false;
 		while (!bIsSorted)
 		{
 			bool bHadToGetModified = false;
-			for (size_t i = 1; i < size; ++i)
+			for (size_t i = 1; i < _size; ++i)
 			{
-				if (arr[i - 1] < arr[i])
+				bool bMatchMethod = _method == ESortMethod::Ascending ? _arr[i - 1] > _arr[i] : _arr[i - 1] < _arr[i];
+				if (!bMatchMethod)
 				{
 					continue;
 				}
-				T tempElement = arr[i];
-				arr[i] = arr[i-1];
-				arr[i-1] = tempElement;
+				T tempElement = std::move(_arr[i]);
+				_arr[i] = _arr[i-1];
+				_arr[i-1] = tempElement;
 				bHadToGetModified = true;
 			}
 			if (!bHadToGetModified)
@@ -48,20 +116,95 @@ namespace AlgoLib
 	}
 
 	template <typename T>
-	static void InsertionSort(T arr[], const size_t& size)
+	static void BubbleSort(T _arr[], const size_t& _size, bool(*_predicate)(T a, T b))
 	{
-		for (size_t i = 1; i < size; ++i)
+		bool bIsSorted = false;
+		while (!bIsSorted)
+		{
+			bool bHadToGetModified = false;
+			for (size_t i = 1; i < _size; ++i)
+			{
+				if (!_predicate(_arr[i-1], _arr[i]))
+				{
+					continue;
+				}
+				T tempElement = std::move(_arr[i]);
+				_arr[i] = _arr[i - 1];
+				_arr[i - 1] = tempElement;
+				bHadToGetModified = true;
+			}
+			if (!bHadToGetModified)
+			{
+				bIsSorted = true;
+			}
+		}
+	}
+
+	template <typename T>
+	static void BubbleSort(T _arr[], const size_t& _size, bool(*_predicate)(const T& a, const T& b))
+	{
+		bool bIsSorted = false;
+		while (!bIsSorted)
+		{
+			bool bHadToGetModified = false;
+			for (size_t i = 1; i < _size; ++i)
+			{
+				if (!_predicate(_arr[i - 1], _arr[i]))
+				{
+					continue;
+				}
+				T tempElement = std::move(_arr[i]);
+				_arr[i] = _arr[i - 1];
+				_arr[i - 1] = tempElement;
+				bHadToGetModified = true;
+			}
+			if (!bHadToGetModified)
+			{
+				bIsSorted = true;
+			}
+		}
+	}
+
+	template <typename T>
+	static void BubbleSort(T _arr[], const size_t& _size, bool(*_predicate)(T& a, T& b))
+	{
+		bool bIsSorted = false;
+		while (!bIsSorted)
+		{
+			bool bHadToGetModified = false;
+			for (size_t i = 1; i < _size; ++i)
+			{
+				if (!_predicate(_arr[i - 1], _arr[i]))
+				{
+					continue;
+				}
+				T tempElement = std::move(_arr[i]);
+				_arr[i] = _arr[i - 1];
+				_arr[i - 1] = tempElement;
+				bHadToGetModified = true;
+			}
+			if (!bHadToGetModified)
+			{
+				bIsSorted = true;
+			}
+		}
+	}
+
+	template <typename T>
+	static void InsertionSort(T _arr[], const size_t& _size, const ESortMethod& _method)
+	{
+		for (size_t i = 1; i < _size; ++i)
 		{
 			for (size_t j = 0; j < i; ++j)
 			{
-				if (arr[i] > arr[j])
+				/*if (_arr[i] (_method == ESortMethod::Ascending) > _arr[j] : <)
 				{
 					continue;
 				}
 				for (size_t k = j; k < i; ++k)
 				{
 
-				}
+				}*/
 			}
 		}
 	}
